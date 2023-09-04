b44e0e1b7065019f2a8953e748eeab6a428dca99
thuanpv <vanthuanjw@gmail.com>
2023-09-04 15:25:28 +0700

    add tailwindcss config for neovim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- key binding mapleader
 vim.g.mapleader = ";"
-vim.opt.winbar = "%=%m %f"
-vim.opt.foldlevelstart = 99
-vim.opt.foldlevel = 20
-vim.opt.foldmethod = "indent"
+
+local opt = vim.opt
+opt.winbar = "%=%m %f"
+
+-- indentation
+opt.foldlevelstart = 99
+opt.foldlevel = 20
+opt.foldmethod = "indent"
+
+-- split window
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
